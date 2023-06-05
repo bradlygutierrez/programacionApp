53c7bc5f22a2b09b4ae72c2c86d71fb6de00dffc
joreims <jorge.garcia14624@est.uca.edu.ni>
2023-06-04 23:03:53 -0600

    agregargastos reparados porfin, soltame el brazo harry no me pegues que me maltratas y pondré quejas

--- Vistas py/AgregarGasto.py
+++ Vistas py/AgregarGasto.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_agregarGasto(QtWidgets.QMainWindow):
+class Ui_AgregarGasto(object):
     def setupUi(self, AgregarGasto):
         AgregarGasto.setObjectName("AgregarGasto")
         AgregarGasto.resize(800, 557)
@@ -29,7 +29,7 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         self.label.setObjectName("label")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(150, 90, 191, 21))
-        self.lineEdit.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit.setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);")
         self.lineEdit.setText("")
         self.lineEdit.setObjectName("lineEdit")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
@@ -39,7 +39,9 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         font.setBold(True)
         font.setWeight(75)
         self.label_2.setFont(font)
-        self.label_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_2.setStyleSheet("border-radius: 10px;color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(30, 130, 111, 21))
@@ -48,20 +50,24 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         font.setBold(True)
         font.setWeight(75)
         self.label_3.setFont(font)
-        self.label_3.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_3.setStyleSheet("border-radius: 10px;color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
         self.label_3.setObjectName("label_3")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_2.setGeometry(QtCore.QRect(150, 130, 191, 51))
-        self.lineEdit_2.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit_2.setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);")
         self.lineEdit_2.setText("")
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(20, 210, 341, 241))
+        self.groupBox_2.setGeometry(QtCore.QRect(20, 210, 341, 231))
         font = QtGui.QFont()
         font.setBold(True)
         font.setWeight(75)
         self.groupBox_2.setFont(font)
-        self.groupBox_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.groupBox_2.setStyleSheet("border-radius: 10px;color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
         self.groupBox_2.setObjectName("groupBox_2")
         self.label_4 = QtWidgets.QLabel(self.groupBox_2)
         self.label_4.setGeometry(QtCore.QRect(10, 50, 81, 21))
@@ -86,21 +92,24 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         self.label_7.setObjectName("label_7")
         self.lineEdit_3 = QtWidgets.QLineEdit(self.groupBox_2)
         self.lineEdit_3.setGeometry(QtCore.QRect(110, 50, 201, 21))
-        self.lineEdit_3.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit_3.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"\n"
+"color: rgb(0, 0, 0);")
         self.lineEdit_3.setText("")
         self.lineEdit_3.setObjectName("lineEdit_3")
         self.lineEdit_4 = QtWidgets.QLineEdit(self.groupBox_2)
         self.lineEdit_4.setGeometry(QtCore.QRect(110, 100, 201, 21))
-        self.lineEdit_4.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit_4.setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);")
         self.lineEdit_4.setText("")
         self.lineEdit_4.setObjectName("lineEdit_4")
         self.lineEdit_5 = QtWidgets.QLineEdit(self.groupBox_2)
-        self.lineEdit_5.setGeometry(QtCore.QRect(110, 150, 221, 21))
-        self.lineEdit_5.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit_5.setGeometry(QtCore.QRect(110, 150, 201, 21))
+        self.lineEdit_5.setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);")
         self.lineEdit_5.setText("")
         self.lineEdit_5.setObjectName("lineEdit_5")
         self.checkBox = QtWidgets.QCheckBox(self.groupBox_2)
         self.checkBox.setGeometry(QtCore.QRect(110, 190, 201, 21))
+        self.checkBox.setStyleSheet("")
         self.checkBox.setObjectName("checkBox")
         self.label_8 = QtWidgets.QLabel(self.groupBox_2)
         self.label_8.setGeometry(QtCore.QRect(10, 190, 31, 21))
@@ -127,7 +136,9 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         font.setBold(True)
         font.setWeight(75)
         self.groupBox_4.setFont(font)
-        self.groupBox_4.setStyleSheet("color: rgb(255, 255, 255);")
+        self.groupBox_4.setStyleSheet("border-radius: 10px;color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
         self.groupBox_4.setObjectName("groupBox_4")
         self.label_6 = QtWidgets.QLabel(self.groupBox_4)
         self.label_6.setGeometry(QtCore.QRect(350, 40, 401, 21))
@@ -136,7 +147,9 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         font.setBold(True)
         font.setWeight(75)
         self.label_6.setFont(font)
-        self.label_6.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_6.setStyleSheet("border-radius: 10px;color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
         self.label_6.setObjectName("label_6")
         self.comboBox_2 = QtWidgets.QComboBox(self.groupBox_4)
         self.comboBox_2.setGeometry(QtCore.QRect(420, 70, 261, 25))
@@ -147,7 +160,7 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         self.comboBox_2.addItem("")
         self.comboBox_2.addItem("")
         self.label_9 = QtWidgets.QLabel(self.centralwidget)
-        self.label_9.setGeometry(QtCore.QRect(220, 10, 51, 51))
+        self.label_9.setGeometry(QtCore.QRect(220, 10, 41, 41))
         font = QtGui.QFont()
         font.setPointSize(24)
         font.setBold(True)
@@ -157,18 +170,47 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         self.label_9.setText("")
         self.label_9.setObjectName("label_9")
         self.groupBox_5 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_5.setGeometry(QtCore.QRect(370, 240, 411, 181))
+        self.groupBox_5.setGeometry(QtCore.QRect(370, 260, 411, 131))
         font = QtGui.QFont()
         font.setBold(True)
         font.setWeight(75)
         self.groupBox_5.setFont(font)
-        self.groupBox_5.setStyleSheet("color: rgb(255, 255, 255);")
+        self.groupBox_5.setStyleSheet("border-radius: 10px;color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
         self.groupBox_5.setObjectName("groupBox_5")
-        self.textBrowser = QtWidgets.QTextBrowser(self.groupBox_5)
-        self.textBrowser.setGeometry(QtCore.QRect(10, 30, 391, 141))
-        self.textBrowser.setStyleSheet("background-color: rgb(21, 15, 48);\n"
-"border-radius: 10px;")
-        self.textBrowser.setObjectName("textBrowser")
+        self.label_11 = QtWidgets.QLabel(self.groupBox_5)
+        self.label_11.setGeometry(QtCore.QRect(20, 80, 111, 21))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_11.setFont(font)
+        self.label_11.setStyleSheet("color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
+        self.label_11.setObjectName("label_11")
+        self.label_10 = QtWidgets.QLabel(self.groupBox_5)
+        self.label_10.setGeometry(QtCore.QRect(20, 40, 81, 21))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_10.setFont(font)
+        self.label_10.setStyleSheet("color: rgb(0, 0, 0);\n"
+"\n"
+"background-color: rgb(168, 168, 168);")
+        self.label_10.setObjectName("label_10")
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.groupBox_5)
+        self.lineEdit_7.setGeometry(QtCore.QRect(140, 40, 191, 21))
+        self.lineEdit_7.setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);")
+        self.lineEdit_7.setText("")
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.groupBox_5)
+        self.lineEdit_6.setGeometry(QtCore.QRect(140, 80, 191, 21))
+        self.lineEdit_6.setStyleSheet("color: rgb(0, 0, 0);background-color: rgb(255, 255, 255);")
+        self.lineEdit_6.setText("")
+        self.lineEdit_6.setObjectName("lineEdit_6")
         self.groupBox_4.raise_()
         self.label.raise_()
         self.lineEdit.raise_()
@@ -211,12 +253,9 @@ class Ui_agregarGasto(QtWidgets.QMainWindow):
         self.comboBox_2.setItemText(0, _translate("AgregarGasto", "Transporte"))
         self.comboBox_2.setItemText(1, _translate("AgregarGasto", "Tecnología"))
         self.comboBox_2.setItemText(2, _translate("AgregarGasto", "Materiales"))
-        self.groupBox_5.setTitle(_translate("AgregarGasto", "Recordatorio"))
-        self.textBrowser.setHtml(_translate("AgregarGasto", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600; color:#ffffff;\"> Al mantener un registro preciso de tus gastos, podrás tener un mayor control sobre tus ingresos y gastos, identificar patrones de gasto, establecer presupuestos y tomar decisiones financieras más informadas en el futuro.</span></p></body></html>"))
+        self.groupBox_5.setTitle(_translate("AgregarGasto", "Beneficiario"))
+        self.label_11.setText(_translate("AgregarGasto", "Nombre"))
+        self.label_10.setText(_translate("AgregarGasto", "ID"))
 import CambiarContra
 
 
@@ -224,7 +263,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     AgregarGasto = QtWidgets.QMainWindow()
-    ui = Ui_agregarGasto()
+    ui = Ui_AgregarGasto()
     ui.setupUi(AgregarGasto)
     AgregarGasto.show()
     sys.exit(app.exec_())
